c56a50ab4e7386b758223559726a2054a1e58e63
Eduardo Cabral <47820549+FerroEduardo@users.noreply.github.com>
2024-08-04 13:09:10 -0300

    add custom session management

--- src/main/java/org/example/pocspringsessionjdbc/SecurityConfig.java
+++ src/main/java/org/example/pocspringsessionjdbc/SecurityConfig.java
@@ -8,26 +8,45 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.CompositeSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.ConcurrentSessionControlAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.session.jdbc.JdbcIndexedSessionRepository;
+import org.springframework.session.security.SpringSessionBackedSessionRegistry;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, ConfigurationPropertiesAutoConfiguration configurationPropertiesAutoConfiguration) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, SessionRegistry sessionRegistry) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(conf -> conf.defaultSuccessUrl("/me", true))
+                .formLogin(form -> form.defaultSuccessUrl("/me", true))
                 .logout(Customizer.withDefaults())
-                .headers(conf -> conf.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
-                .sessionManagement(conf -> conf.sessionCreationPolicy(SessionCreationPolicy.NEVER))
-                .authorizeHttpRequests(conf -> conf
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .requestCache(reqCache -> {
+                            HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
+                            requestCache.setCreateSessionAllowed(false);
+                            reqCache.requestCache(requestCache);
+                        }
+                )
+                .sessionManagement(conf -> conf
+                        .sessionCreationPolicy(SessionCreationPolicy.NEVER)
+                        .sessionAuthenticationStrategy(getAuthenticationStrategy(sessionRegistry))
+                )
+                .authorizeHttpRequests(req -> req
                         .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/auth/sign-in").permitAll()
                         .anyRequest().authenticated()
                 );
 
@@ -35,7 +54,25 @@ public class SecurityConfig {
     }
 
     @Bean
-    PasswordEncoder passwordEncoder() {
+    public SessionRegistry sessionRegistry(JdbcIndexedSessionRepository sessionRepository) {
+        return new SpringSessionBackedSessionRegistry<>(sessionRepository);
+    }
+
+    private CompositeSessionAuthenticationStrategy getAuthenticationStrategy(SessionRegistry sessionRegistry) {
+        ConcurrentSessionControlAuthenticationStrategy concurrentSessionControlStrategy = new ConcurrentSessionControlAuthenticationStrategy(sessionRegistry);
+        concurrentSessionControlStrategy.setMaximumSessions(1);
+        concurrentSessionControlStrategy.setExceptionIfMaximumExceeded(true);
+
+        RegisterSessionAuthenticationStrategy registerSessionStrategy = new RegisterSessionAuthenticationStrategy(sessionRegistry);
+
+        return new CompositeSessionAuthenticationStrategy(List.of(
+                concurrentSessionControlStrategy,
+                registerSessionStrategy
+        ));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
